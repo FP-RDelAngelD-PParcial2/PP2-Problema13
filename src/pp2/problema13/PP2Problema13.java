b65f3f980d35e829f43bc308b6ec4ad9e9ea9269
PC <PC@192.168.1.70>
2016-10-15 17:27:55 -0500

    13.	Realiza un programa que lea 30 enteros, los guarde en un arreglo, calcule su promedio, determine la cantidad de elementos que están por encima del promedio y la cantidad de elementos que están por debajo del promedio, y muestre los resultados.

--- src/pp2/problema13/PP2Problema13.java
+++ src/pp2/problema13/PP2Problema13.java
@@ -15,8 +15,10 @@ public class PP2Problema13 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //Declaración de variables
         int[]numero = new int[30];
         double promedio;
+        //Metodos
         promedio=mostrarVector(llenarVector(numero));
         sobrePromedio(promedio,numero);
         bajoPromedio(promedio,numero);
@@ -32,6 +34,7 @@ public class PP2Problema13 {
         return n;
        }
     public static double mostrarVector(int []n){ //El arreglo entra en el metodo
+        //Declaración de variables
         double promedio;
         int numerospromedio;
         numerospromedio=0;
@@ -40,28 +43,35 @@ public class PP2Problema13 {
                                         //i regresa al for y como es menor se le suma 1 y baja de nuevo
                                         //, el valor de la posicion se suma a promedio
             }
-        promedio=numerospromedio/n.length;
-        System.out.print("El promedio es: " + promedio);
+        promedio=numerospromedio/n.length; //Se divide los numerospromedio (numeros sumados del arreglo) entre el tamaño del arreglo(30)
+                                           //Se guarda en la variable promedio
+        System.out.print("El promedio es: " + promedio); //Se imprime el promedio
         return promedio;
         }
     public static void sobrePromedio(double promedio, int[]n){
+       //Declaración de variables
         int numerossobrepromedio;
         numerossobrepromedio=0;
-        for(int i=0;i<n.length;i++){
-            if(n[i]>promedio){
-                numerossobrepromedio++;
+        for(int i=0;i<n.length;i++){ //Mientras que i sea menor que el tamaño del arreglo(30), i aumentara 1
+            if(n[i]>promedio){       //Si el valor de la posicion del arreglo es mayor al promedio
+                numerossobrepromedio++;//La variable numerossobrepromedio aumentara 1
+                                       //El contador continua hasta que alcanza 30
             }
         }
+        //Se imprime la cantidad de veces que se aumento numerossobrepromedio
         System.out.print("\n" + "La cantidad de elementos que estan por encima del promedio son: " + numerossobrepromedio);
     }
     public static void bajoPromedio(double promedio, int[]n){
+        //Declaración de variables
         int numerosbajopromedio;
         numerosbajopromedio=0;
-        for(int i=0;i<n.length;i++){
-            if(n[i]<promedio){
-                numerosbajopromedio++;
+        for(int i=0;i<n.length;i++){ //Mientras que i sea menor que el tamaño del arreglo(30), i aumentara 1
+            if(n[i]<promedio){       //Si el valor de la posicion del arreglo es menor al promedio
+                numerosbajopromedio++;//La variable numerosbajopromedio aumentara 1
+                                      //El contador continua hasta que alcanza 30
             }
         }
+        //Se imprime la cantidad de veces que se aumento numerosbajopromedio
         System.out.print("\n" + "La cantidad de elementos que estan por debajo del promedio son: " + numerosbajopromedio);
     }
 }
