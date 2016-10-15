18b0264c5e39fda8127a1f12e14b91c21112f926
PC <PC@192.168.1.70>
2016-10-15 16:20:48 -0500

    13.	Realiza un programa que lea 30 enteros, los guarde en un arreglo, calcule su promedio, determine la cantidad de elementos que están por encima del promedio y la cantidad de elementos que están por debajo del promedio, y muestre los resultados.

--- src/pp2/problema13/PP2Problema13.java
+++ src/pp2/problema13/PP2Problema13.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package pp2.problema13;
-
+import java.util.*;
 /**
  *
  * @author PC
@@ -15,7 +15,17 @@ public class PP2Problema13 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int[]numero = new int[30];
         
     }
-    
+    public static int [] llenarVector(int []n){//Metodo de llenar el vector, recibe un arreglo de enteros, regresa un arreglo de enteros
+        Scanner entradaEscaner = new Scanner(System.in);       
+        for (int i = 0; i<n.length; i++){//Mientras que el valor de i sea menor que el tamaño del arreglo(30), i se aumentara en 1
+            System.out.print("Introduce un número para la posición [" + i + "]"); //i indica la posición
+            n[i]=entradaEscaner.nextInt();//En la pocision i se asigna el valor
+                                          //Y regresa hasta que la posicion i se vuelva 10
+                                          //En ese momento regresa el arreglo
+        }
+        return n;
+       }
 }
