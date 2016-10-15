089d0e5a547192352b9e638601b9a6a0e7d3afc0
PC <PC@192.168.1.70>
2016-10-15 15:54:03 -0500

    13.	Realiza un programa que lea 30 enteros, los guarde en un arreglo, calcule su promedio, determine la cantidad de elementos que están por encima del promedio y la cantidad de elementos que están por debajo del promedio, y muestre los resultados.

--- src/pp2/problema13/PP2Problema13.java
+++ src/pp2/problema13/PP2Problema13.java
@@ -15,7 +15,7 @@ public class PP2Problema13 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
